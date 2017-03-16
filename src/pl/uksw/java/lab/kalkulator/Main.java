71aeed906253fa19c4d1ade685eb88549c52fd40
Przemek <Przemek@DESKTOP-36BAQT9>
2017-03-16 22:28:24 +0100

    Dodanie Javadoc do wszystkich metod.

--- src/pl/uksw/java/lab/kalkulator/Main.java
+++ src/pl/uksw/java/lab/kalkulator/Main.java
@@ -1,20 +1,23 @@
 /**
- * Powo³uje obiekt typu Menu i pozwala mu dzia³aæ
+ * Pakiet dla aplikacji kalkulator
  */
 package pl.uksw.java.lab.kalkulator;
 
 /**
  * @author Przemys³aw Gontarz
- *
+ * Powo³uje obiekt typu Menu i pozwala mu dzia³aæ
  */
 public class Main {
 
 	/**
-	 * @param args
+	 * @param args Argumenty pobierane
 	 */
 	public static void main(String[] args) {
+		/**
+		 * Obiekt wyœwietlaj¹cy interfejs u¿ytkownika
+		 */
 		Menu menu = new Menu();
-		menu.printMenu();
+		menu.run();
 	}
 
 }
--- src/pl/uksw/java/lab/kalkulator/Menu.java
+++ src/pl/uksw/java/lab/kalkulator/Menu.java
@@ -5,19 +5,43 @@ package pl.uksw.java.lab.kalkulator;
 
 /**
  * @author Przemys³aw Gontarz
- *
+ * Klasa obs³uguj¹ca interfejs u¿ytkownika
  */
 public class Menu 
 {
+	/**
+	 * Pole przechowuj¹ce ostatni wybór u¿ytkownika. Definiuje tryb pracy kalkulatora
+	 */
+	private int userOption;
+	/**
+	 * Konstruktor - obecnie brak dzia³añ
+	 */
 	public Menu()
 	{
 		
 	}
-	public void printMenu() {
+	/**
+	 * Metoda odczytuj¹ca wybór u¿ytkownika
+	 */
+	private void readOption() {
+		
+	}
+	/**
+	 * Metoda organizuj¹ca sterowanie programem. Zawiera wywo³anie poszczególnych funkcji programu oraz steruje jego dzia³aniem
+	 */
+	public void run()
+	{
+		printMenu();
+	}
+	/**
+	 * Wyœwietlenie menu pozwalaj¹cego na wybór trybu pracy Kalkulatora
+	 */
+	private void printMenu() {
 		System.out.println();
 		System.out.println("Menu programu KALKULATOR: ");
 		System.out.println("Wybierz tryb dzia³ania kalkulatora: ");
 		System.out.println("1. W konsoli");
 		System.out.println("2. Z pliku");
+		System.out.println("Wybór: ");
 	}
 }
