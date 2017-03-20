1d76ddb0301b1ac3aa7d5affd01b944206a2f7de
Przemek <Przemek@DESKTOP-36BAQT9>
2017-03-20 21:40:09 +0100

    Obsługa wyboru trybu pracy programu. Aktualizacja javadoc

--- src/pl/uksw/java/lab/kalkulator/Menu.java
+++ src/pl/uksw/java/lab/kalkulator/Menu.java
@@ -3,6 +3,8 @@
  */
 package pl.uksw.java.lab.kalkulator;
 
+import java.util.Scanner;
+
 /**
  * Klasa obs³uguj¹ca interfejs u¿ytkownika
  * @author Przemys³aw Gontarz
@@ -14,17 +16,47 @@ public class Menu
 	 */
 	private int userOption;
 	/**
-	 * Konstruktor - obecnie brak dzia³añ
+	 * Bufor wejœciowy klawiatury
+	 */
+	private Scanner scanner;
+	/**
+	 * Ustawia wartoœci domyœlne
 	 */
 	public Menu()
 	{
-		
+		setUserOption(0);
+	}
+	/**
+	 * Zwraca wartoœæ pola userOption
+	 * @return Wartoœæ typu ca³kowitego, ostatni wybór u¿ytkownika dotycz¹cy trybu pracy kalkulatora
+	 */
+	public int getUserOption() {
+		return userOption;
+	}
+	/**
+	 * Ustawienie pola userOption zgodnie z iloœci¹ dostêpnych trybów pracy
+	 * @param userOption Obecnie s¹ dwa tryby pracy. Jeœli zostanie przekazana inna wartoœæ, zostanie zast¹pion¹ wartoœci¹ domyœlna (0)
+	 */
+	public void setUserOption(int userOption) {
+		if (userOption ==1)
+		{
+			this.userOption = 1;
+		}
+		else if (userOption == 2)
+		{
+			this.userOption = 2;
+		}
+		else
+		{
+			this.userOption = 0;
+		}
 	}
 	/**
 	 * Metoda odczytuj¹ca wybór u¿ytkownika
 	 */
 	private void readOption() {
-		
+		scanner = new Scanner(System.in);
+		setUserOption(scanner.nextInt());	
 	}
 	/**
 	 * Metoda organizuj¹ca sterowanie programem. Zawiera wywo³anie poszczególnych funkcji programu oraz steruje jego dzia³aniem
@@ -32,6 +64,9 @@ public class Menu
 	public void run()
 	{
 		printMenu();
+		readOption();
+		System.out.println();
+		System.out.println("Koniec programu.");
 	}
 	/**
 	 * Wyœwietlenie menu pozwalaj¹cego na wybór trybu pracy Kalkulatora
@@ -42,6 +77,6 @@ public class Menu
 		System.out.println("Wybierz tryb dzia³ania kalkulatora: ");
 		System.out.println("1. W konsoli");
 		System.out.println("2. Z pliku");
-		System.out.println("Wybór: ");
+		System.out.print("Wybór: ");
 	}
 }
