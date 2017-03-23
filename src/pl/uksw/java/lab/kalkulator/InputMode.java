058ca7df1ae1a7b7b362bd3f14d88639bdcfa064
Przemysław Gontarz <przemyslaw.gontarz@student.uksw.edu.pl>
2017-03-23 19:53:45 +0100

    Dodanie metody read() w interfejsie InputMode

--- src/pl/uksw/java/lab/kalkulator/InputMode.java
+++ src/pl/uksw/java/lab/kalkulator/InputMode.java
@@ -4,9 +4,13 @@
 package pl.uksw.java.lab.kalkulator;
 
 /**
- * @author Przemek
+ * Interfejs dla ró¿nych trybów wprowadzania danych
+ * @author Przemys³aw Gontarz 87850
  *
  */
 public interface InputMode {
-
+	/**
+	 * Metoda wprowadzaj¹ca odczytuj¹ca dane wejœciowe
+	 */
+	public void read();
 }
