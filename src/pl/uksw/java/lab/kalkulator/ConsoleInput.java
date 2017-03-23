09bbcaa6734fef773cc46afd626a037f6a020551
Przemysław Gontarz <przemyslaw.gontarz@student.uksw.edu.pl>
2017-03-23 19:56:52 +0100

    Utworzenie metody klasy ConsoleInput - odczyt z konsoli

---
+++ src/pl/uksw/java/lab/kalkulator/ConsoleInput.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package pl.uksw.java.lab.kalkulator;
+
+/**
+ * Klasa obs³uguj¹ca wprowadzanie przez konsole
+ * @author Przemek
+ *
+ */
+public class ConsoleInput implements InputMode {
+
+	/* (non-Javadoc)
+	 * @see pl.uksw.java.lab.kalkulator.InputMode#read()
+	 */
+	@Override
+	public void read() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
