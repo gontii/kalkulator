f276ebd27a67c31beec9a8df7ecc5e99f870cc37
Przemysław Gontarz <przemyslaw.gontarz@student.uksw.edu.pl>
2017-03-24 19:20:05 +0100

    Dodana klasa FileInput, obsłużone otwieranie pliku bez dalszych działań

--- src/pl/uksw/java/lab/kalkulator/Main.java
+++ src/pl/uksw/java/lab/kalkulator/Main.java
@@ -3,6 +3,8 @@
  */
 package pl.uksw.java.lab.kalkulator;
 
+import java.io.FileNotFoundException;
+
 /**
  * Powo³uje obiekt typu Menu i pozwala mu dzia³aæ
  * @author Przemys³aw Gontarz
@@ -12,8 +14,9 @@ public class Main {
 
 	/**
 	 * @param args Argumenty pobierane
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		/**
 		 * Obiekt wyœwietlaj¹cy interfejs u¿ytkownika
 		 */
